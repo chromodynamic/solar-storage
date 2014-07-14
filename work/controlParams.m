7640b766fa112a7450da98822ed30e8bb1bb69bd
Jonathan LeSage <jrlesage@gmail.com>
2014-07-13 23:54:45 -0400

    Parameterized the battery power limit logic in Stateflow

--- work/controlParams.m
+++ work/controlParams.m
@@ -1,9 +1,16 @@
 %% Battery Charge/Discharge Values
 
+% Max/min SOC
+maxSOC = 80;
+minSOC = 20;
+
+% SOC Hysteresis threshold
+hysteresisSOC = 5;
+
 % Charge Conditions
 finalVoltage = 30;      % Desired Final Cell Voltage [V]
 currentSat = 0.5;       % Current Saturation (Charge off) [A]
-maxChargeRate = 5;      % Constant Current Charge Rate [A]
+maxChargeRate = 6;      % Constant Current Charge Rate [A]
 
 % Discharge Conditions
 maxDischargeRate = -5;  % Maximum Discharge Rate [A]
@@ -11,3 +18,7 @@ maxDischargeRate = -5;  % Maximum Discharge Rate [A]
 % Bus Regulation
 % desiredBusVoltage = 24.45; % Desired DC bus voltage [V]
 desiredBusVoltage = 30; % Desired DC bus voltage [V]
+
+% Power hysteresis threshold [W]
+powerHysteresis = 10;
+
